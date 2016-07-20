759806081451b7e7b568a7f7f33e9f55bc0011ff
CaiJiang <luffy.ja@gmail.com>
2016-07-20 23:23:19 +0800

    and more

--- wildfly/install.sh
+++ wildfly/install.sh
@@ -7,10 +7,10 @@ if [ $UID -ne 0 ]; then
     exit 1
 fi
 
-wget http://resali.huobanplus.com/wildfly-10.0.0.Final.zip
+wget http://resali.huobanplus.com/wildfly-10.0.0.Final.tar.gz
 # 获取当前目录
-unzip wildfly-10.0.0.Final.zip
-rm wildfly-10.0.0.Final.zip
+tar -xzf wildfly-10.0.0.Final.tar.gz
+rm wildfly-10.0.0.Final.tar.gz
 
 PWD=`pwd`
 HOME=${PWD}/wildfly-10.0.0.Final
@@ -18,7 +18,12 @@ HOME=${PWD}/wildfly-10.0.0.Final
 # 添加用户
 useradd -c "Wildfly Daemon User" -r -d "${HOME}" wildfly
 
+chown -R wildfly:wildfly $HOME
+
 echo "export WILDFLY_USER=wildfly" >> /etc/environment
 echo "export WILDFLY_HOME=$HOME" >> /etc/environment
 
 echo "it's very importment to true resolve-parameter-values in ${HOME}/bin/jboss-cli.xml"
+
+# 要制作一个启动的脚本呀，笨
+# 再制作 2个 用户专用 启动脚本 和cli脚本
