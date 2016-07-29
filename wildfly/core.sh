6785b3f4329c103ef31e2a4eacba042229fa6492
CaiJiang <luffy.ja@gmail.com>
2016-07-29 16:29:12 +0800

    deploy cms

--- wildfly/core.sh
+++ wildfly/core.sh
@@ -25,17 +25,26 @@ function execCLI(){
 
   TMP="$(mktemp -q -t "$(basename "$0").XXXXXX" 2>/dev/null || mktemp -q)"
 
-  while [[ $2 ]]; do
+  while [[ $2 && $3 ]]; do
     # 读取 $2和$3
     echo "$2=$3" >> $TMP
     shift
     shift
   done
+
+  # 最后一个孤单的参数表示远程地址
+  Remote=$2
+  echo "Remote=" $2
+  RemotePadding=""
+  if [[ $Remote ]]; then
+    RemotePadding="--controller="$Remote
+  fi
+
   if [[ ${WILDFLY_HOME_MANAGEMENT_USER} ]]; then
-    $CLISH --properties=$TMP --file=$CLI -c -u=$WILDFLY_HOME_MANAGEMENT_USER -p=$WILDFLY_HOME_MANAGEMENT_PASSWORD
+    $CLISH --properties=$TMP --file=$CLI -c -u=$WILDFLY_HOME_MANAGEMENT_USER -p=$WILDFLY_HOME_MANAGEMENT_PASSWORD $RemotePadding
   else
     echo "setup WILDFLY_HOME_MANAGEMENT_USER WILDFLY_HOME_MANAGEMENT_PASSWORD vars in .bash_profile to work fast."
-    $CLISH --properties=$TMP --file=$CLI -c
+    $CLISH --properties=$TMP --file=$CLI -c $RemotePadding
   fi
 
   rm $TMP
