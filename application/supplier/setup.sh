fba9fb3b5c352806afd7a7e30b0ca1d32263ad75
helloztt <347871727@qq.com>
2017-09-18 14:17:21 +0800

    bug

--- application/supplier/setup.sh
+++ application/supplier/setup.sh
@@ -33,11 +33,11 @@ Owner=$3
 
 # cdn 添加资源
 mkdir -p /data1/cdn/hotsupplier
-chown Owner:Owner /data1/cdn/hotsupplier
+chown ${Owner}:${Owner} /data1/cdn/hotsupplier
 
 # 开始处理 tomcat
 cp ${ScriptHome}/context.xml ${Tomcat_home}/conf/Catalina/localhost/context.xml.default
 sed -i -e "s@#{domain}@"${Domain}"@g"\
  ${Tomcat_home}/conf/Catalina/localhost/context.xml.default
 chmod +r ${Tomcat_home}/conf/Catalina/localhost/context.xml.default
-chown Owner:Owner ${Tomcat_home}/conf/Catalina/localhost/context.xml.default
+chown ${Owner}:${Owner} ${Tomcat_home}/conf/Catalina/localhost/context.xml.default
